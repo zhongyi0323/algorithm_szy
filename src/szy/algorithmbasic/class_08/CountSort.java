f0ac958b26627a69d7def472d25a65b4d3d10f9e
sunzhongyi <sunzhongyi@didachuxing.com>
2021-03-14 21:18:21 +0800

    计数、基数排序

---
+++ src/szy/algorithmbasic/class_08/CountSort.java
@@ -0,0 +1,27 @@
+package szy.algorithmbasic.class_08;
+
+/**
+ * 计数排序
+ */
+public class CountSort {
+
+    //only for 0~200 age
+    public static void countSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int[] help = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            help[arr[i]]++;
+        }
+        int i = 0;
+        for (int j = 0; j < help.length; j++) {
+            while (help[j]-->0){
+                arr[i++] = j;
+            }
+        }
+    }
+
+}
